39bdac41839a2f8c259f622e39917d4ee3deba92
Zanfer <zanfer@zanfertech.com>
2019-01-03 12:40:25 +0000

    new peak scripts finished

--- multipeak.py
+++ multipeak.py
@@ -2,13 +2,11 @@
 
 AR = (3, 89, 222, 78, 966,34, 5867, 34566, 7456, 23, 1000, 465, 38, 22, 55)
 
-PEAK = [AR[0]]
+PEAK = []
 
 count = 0
 while count < len(AR):
-    if (count + 1) == len(AR):
-        break
-    if count == 0 or AR[count] > PEAK or AR[(count - 1)] < AR[count] and AR[(count + 1)]:
+    if count == 0 and AR[0] > AR[1] or (count == len(AR) -1 and AR[count] > AR[count-1]) or (AR[count] > AR[count - 1] and AR[count] > AR[count+1]):
         PEAK.append(AR[count])
     count += 1
 
