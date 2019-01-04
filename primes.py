6bfb042120857ecb1828f4fdad8d4976ef4b64f9
Zanfer <zanfer@zanfertech.com>
2019-01-04 03:38:36 +0000

    primes complete

--- primes.py
+++ primes.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3.6
 
 PRIMES = []
+flag = []
 
-for x in range(0,200):
-    for n in range(0,10):
-        if (x % n) != 0:
-            print("{} is prime".format(x))
-            PRIMES.append(x)
+for x in range(2,200):
+    flag = []
+    for n in range(2,10):
+        if x == n:
+            continue
+        elif (x % n) == 0:
+            flag.append(n)
+    if len(flag) == 0:
+        print("{} is prime".format(x))
+        PRIMES.append(x)
 
 print(PRIMES)
