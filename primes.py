bfcf1ce3848056c33d83371cc2601c341068cffc
Zanfer <zanfer@zanfertech.com>
2019-01-04 03:03:09 +0000

    finished writing primes

--- primes.py
+++ primes.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3.6
 
+PRIMES = []
 
+for x in range(0,200):
+    for n in range(0,10):
+        if (x % n) != 0:
+            print("{} is prime".format(x))
+            PRIMES.append(x)
+
+print(PRIMES)
