eb3dd4e4b8bff4eb44f397f6ac11b8cfcf253bbf
Zanfer <zanfer@zanfertech.com>
2019-01-05 08:05:38 +0000

    fastest known version

--- primes.py
+++ primes.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3.6
 
+import math
+
 PRIMES = []
 
 for x in range(2,10000):
     no_remainder = False
-    for n in range(2,int((x/2)+1)):
+    for n in range(2,int(math.sqrt(x) + 1 )):
         if (x % n) == 0:
             no_remainder = True
+            break
             #flag.append(n)
     if not no_remainder:
         #debug# print("{} is prime".format(x))
