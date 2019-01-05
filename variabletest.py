3f41f388691a7e74fe2d74e0710e9000578a0ed5
Zanfer <zanfer@zanfertech.com>
2019-01-05 05:33:24 +0000

    Homework hahah

---
+++ variabletest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+def main():
+    info = { "f_n" : "Zanfer", "l_n" : "Ali", "age" : 35, "birthdate" : "1/5/1984"}
+
+    p = "."
+    for x in range(1, 8):
+        print(p)
+        p += p
+
+    print(f"Hi!  My name is {info['f_n']} {info['l_n']} and it's my birthday!!")
+    print(f"I was born on this day, {info['birthdate']}, and I am now {info['age']} years old :) ")
+
+    x = len(p)//2
+    while x > 0:
+        print(p[:x])
+        x = (x//2)
+
+if __name__ == '__main__':
+    main()
