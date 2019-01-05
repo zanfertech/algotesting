73da49bc67b5c8619f717557a3a720e7567d7bf4
Zanfer <zanfer@zanfertech.com>
2019-01-05 01:14:43 +0000

    figured out ttt is not pulling in list or tuple type

--- tictactoe.py
+++ tictactoe.py
@@ -3,10 +3,12 @@
 import os
 
 def main():
+    t = []
     t = open("/home/cloud_user/bin/toe.txt", "r")
-
+    print(f"t is type {type(t)}")
     for x in t:
-        print(len(x))
+        print(f"x is legth {len(x)}")
+        print(f"x is type {type(x)}")
         for n in range(0,3):
             print(x[n])
 
