d9f6e3864ae380e131aee95cb3c1cc5cade0f12b
Zanfer <zanfer@zanfertech.com>
2019-01-05 08:29:13 +0000

    got rid of for looops

--- primes.py
+++ primes.py
@@ -2,17 +2,24 @@
 
 import math
 
-PRIMES = []
+PRIMES = [2]
 
-for x in range(2,10000):
+#for x in range(3,10000,2)
+x = 3
+while x < 10000:
     no_remainder = False
-    for n in range(2,int(math.sqrt(x) + 1 )):
-        if (x % n) == 0:
+    #for n in range(2,int(math.sqrt(x) + 1 )):
+    i = 0
+    s = math.sqrt(x)
+    while PRIMES[i] <= s and i < len(PRIMES):
+        if (x % PRIMES[i]) == 0:
             no_remainder = True
             break
+        i += 1
             #flag.append(n)
     if not no_remainder:
         #debug# print("{} is prime".format(x))
         PRIMES.append(x)
+    x+=2
 
 print(PRIMES)
