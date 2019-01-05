ad8249bedfc2ff0f3e1bf1b23bd0eaff8c528cf0
Zanfer <zanfer@zanfertech.com>
2019-01-05 06:04:07 +0000

    last HW ass

---
+++ enhanced_userdata.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+
+def main():
+    user = { 'admin' : True , 'active' : True , 'name' : 'Kevin' }
+
+    if user['admin'] and user['active']:
+        print(f"ACTIVE - (ADMIN) {user['name']}")
+    elif user['active'] and not user['admin']:
+        print(f"ACTIVE - {user['name']}")
+    elif user['admin'] and not user['active']:
+        print(f"(ADMIN) {user['name']}")
+    else:
+        print(user['name'])
+
+
+
+if __name__ == "__main__":
+    main()
