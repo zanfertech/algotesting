1e32b868a56a5f064227583a54ae0b108156014a
Zanfer <zanfer@zanfertech.com>
2019-01-06 09:20:35 +0000

    game over

---
+++ guessinggame.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.6
+
+print("Hellooooo, I want to play a little game")
+print("")
+print("Pick a number from 1 - 1000")
+
+
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+def main():
+    lrange = 1
+    hrange = 1000
+    i = 2
+    while i != 0:
+        num = int((lrange + hrange) / 2)
+        print("")
+        i = int(input(f"Is you number {num}? Enter 0 if it is; 1 if it is more than {num}; -1 if it s less than {num}: "))
+
+        if i == -1:
+            hrange = num
+        elif i == 1:
+            lrange = num
+        else:
+            print("Horraaayyyyyyy")
+
+if __name__ == "__main__":
+    main()
+
