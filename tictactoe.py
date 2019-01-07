45b566719037ee2babbc05de6a18cb8d74dfda9e
Zanfer <zanfer@zanfertech.com>
2019-01-07 09:14:48 +0000

    started algo for ttt

--- tictactoe.py
+++ tictactoe.py
@@ -2,28 +2,20 @@
 
 import os #gonna read from file cause its cool
 
-<<<<<<< HEAD
-
 def main():
-    t = open("/home/cloud_user/bin/toe.txt", "r")
 
-    for x in t:
-        print(len(x))
-        for n in range(0,3):
-            print(x[n])
+    tx = ['x', 'x', 'x' ]
+    ty = ['x', 'E', 'o' ]
+    tz = ['x', 'E', 'E' ]
 
-if __name__ == '__main__':
-=======
-def main():
-    t = []
-    t = open("/home/cloud_user/bin/toe.txt", "r")
-    print(f"t is type {type(t)}")
-    for x in t:
-        print(f"x is legth {len(x)}")
-        print(f"x is type {type(x)}")
-        for n in range(0,3):
-            print(x[n])
+
+    if tx[0] == tx[1] == tx[2] or ty[0] == ty[1] == ty[2] or tz[0] == tz[1] == tz[2]:
+
+
+    elif tx[0] == ty[0] == tz[0] or tx[1] == ty[1] == tz[1] or tx[2] == ty[2] == tz[2]:
+        print("TicTacToe")
+    else:
+        print("try again")
 
 if __name__ == "__main__":
->>>>>>> master
     main()
