104ea9f4e8646e7f3f38b7e2d45e080dcf8c0483
zanfertech <zanfer@nettempo.com>
2019-01-19 20:15:05 -0800

    starting weather script

--- ftest.py
+++ ftest.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3.6
+
+import argparse
+import os
+
+count = int(os.environ["TESTING"])
+parser = argparse.ArgumentParser(description="Just tellin you like it iees")
+parser.add_argument('filename', help='put in the file name here')
+parser.add_argument('after')
+
+print(f"this is rotation {count}")
+thisparse = parser.parse_args()
+print(f"{thisparse.filename} is what I said before {thisparse.after}")
---
+++ weather.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.6
+
+import requests
+import sys
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
